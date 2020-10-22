a0158e3734c598739a5d079e9fda7278ef3d618f
Seathorne <scottketclarke@gmail.com>
2020-10-21 22:27:18 -0500

    Updated formatting rules
    Modified some formatting rules to more closely align with the Java formatting used in the book (mainly aligned braces).

--- src/battalions/Driver.java
+++ src/battalions/Driver.java
@@ -9,13 +9,15 @@ package battalions;
  *
  * @author scott
  */
-public class Driver {
+public class Driver
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO code application logic here
     }
-    
+
 }
