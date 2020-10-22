e37d9d43dbe6fbb47870b3638b86891708937b02
Seathorne <scottketclarke@gmail.com>
2020-10-21 22:28:01 -0500

    Added test package
    Added test package and example JUnit test.

---
+++ test/battalions/DriverTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battalions;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author scott
+ */
+public class DriverTest
+{
+
+    public DriverTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    /**
+     * Test of main method, of class Driver.
+     */
+    @Test
+    public void testMain()
+    {
+        System.out.println("main");
+        String[] args = null;
+        Driver.main(args);
+
+        assertTrue(true);
+    }
+
+}
